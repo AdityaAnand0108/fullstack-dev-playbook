70dea9f2e0998fc19c976d74f629af000ae764f5
AdityaAnand0108 <129715108+AdityaAnand0108@users.noreply.github.com>
2025-10-22 17:57:51 +0530

    Add Card component and update imports structure
    Introduced a new Card component to display user information and moved UseStateSimple hook to a new directory for better organization. Updated App.jsx to use the new Card component and reflect the new import paths.

--- react_learning/src/App.jsx
+++ react_learning/src/App.jsx
@@ -1,9 +1,12 @@
-import Usestate from "./Hooks/UseStateSimple";
+import Usestate from "./component/Hooks/UseStateSimple.jsx";
+import Card from "./component/Cards.jsx";
+
 function App() {
 
   return (
     <>
       < Usestate />
+      < Card username="Aditya Anand Mishra" introduction="I’m Aditya Anand Mishra, a Full Stack Developer with over 4 years of experience in designing and developing scalable, enterprise-grade web applications using React.js, Java, Spring Boot, and MySQL. I specialize in end-to-end application development — from architecting backend services and optimizing databases to crafting responsive, high-performance frontends." />
     </>
   )
 }
