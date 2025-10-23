36d22e2bbe78edbf9fced38390859468d57c3af6
AdityaAnand0108 <129715108+AdityaAnand0108@users.noreply.github.com>
2025-10-23 19:42:19 +0530

    Refactor App layout and simplify CSS
    Updated App.jsx to improve layout with flex utilities, enhanced button styles, and cleaned up component structure. Simplified index.css by removing custom styles, relying solely on Tailwind CSS for styling.

--- react_learning/src/App.jsx
+++ react_learning/src/App.jsx
@@ -3,17 +3,34 @@ import Card from "./component/Cards.jsx";
 import { useState } from "react";
 
 function App() {
-
-  const [color, setColor] = useState("white")
+  const [color, setColor] = useState("white");
 
   return (
-      <div className="text-center text-3xl font-bold underline w-full h-lvh" style={{backgroundColor: color}}>
-        <button className="bg-red-100 text-white p-2 m-2 rounded-b-xl" onClick={() => setColor("Red")}>Red</button>
-        <button className="bg-green-100 text-white p-2 m-2 rounded-b-xl" onClick={() => setColor("Green")}>Green</button>
-        < Usestate />
-        < Card username="Aditya Anand Mishra" introduction="I’m Aditya Anand Mishra, a Full Stack Developer with over 4 years of experience in designing and developing scalable, enterprise-grade web applications using React.js, Java, Spring Boot, and MySQL. I specialize in end-to-end application development — from architecting backend services and optimizing databases to crafting responsive, high-performance frontends." />
-      </div>
-  )
+    <div
+      className="text-center text-3xl font-bold w-full min-h-screen flex flex-col items-center justify-center"
+      style={{ backgroundColor: color }}
+    >
+      <button
+        className="bg-red-800 text-white p-6 m-6 rounded-xl"
+        onClick={() => setColor("Red")}
+      >
+        Red
+      </button>
+      <button
+        className="bg-green-800 text-white p-6 m-6 rounded-xl"
+        onClick={() => setColor("Green")}
+      >
+        Green
+      </button>
+      <Usestate />
+      <Card
+        username="Aditya Anand Mishra"
+        introduction="I’m Aditya Anand Mishra, a Full Stack Developer with over 4 years of experience in designing and developing scalable, enterprise-grade web applications 
+        using React.js, Java, Spring Boot, and MySQL. I specialize in end-to-end application development — from architecting backend services and optimizing databases to 
+        crafting responsive, high-performance frontends."
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
