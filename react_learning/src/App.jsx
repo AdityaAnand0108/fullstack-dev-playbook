e41e09d9533ff5a31920826290c3b8b2f245c316
AdityaAnand0108 <129715108+AdityaAnand0108@users.noreply.github.com>
2025-10-23 19:36:34 +0530

    Add color change buttons to App component
    Introduced useState to manage background color in the App component. Added Red and Green buttons that update the background color when clicked.

--- react_learning/src/App.jsx
+++ react_learning/src/App.jsx
@@ -1,13 +1,18 @@
 import Usestate from "./component/Hooks/UseStateSimple.jsx";
 import Card from "./component/Cards.jsx";
+import { useState } from "react";
 
 function App() {
 
+  const [color, setColor] = useState("white")
+
   return (
-    <>
-      < Usestate />
-      < Card username="Aditya Anand Mishra" introduction="I’m Aditya Anand Mishra, a Full Stack Developer with over 4 years of experience in designing and developing scalable, enterprise-grade web applications using React.js, Java, Spring Boot, and MySQL. I specialize in end-to-end application development — from architecting backend services and optimizing databases to crafting responsive, high-performance frontends." />
-    </>
+      <div className="text-center text-3xl font-bold underline w-full h-lvh" style={{backgroundColor: color}}>
+        <button className="bg-red-100 text-white p-2 m-2 rounded-b-xl" onClick={() => setColor("Red")}>Red</button>
+        <button className="bg-green-100 text-white p-2 m-2 rounded-b-xl" onClick={() => setColor("Green")}>Green</button>
+        < Usestate />
+        < Card username="Aditya Anand Mishra" introduction="I’m Aditya Anand Mishra, a Full Stack Developer with over 4 years of experience in designing and developing scalable, enterprise-grade web applications using React.js, Java, Spring Boot, and MySQL. I specialize in end-to-end application development — from architecting backend services and optimizing databases to crafting responsive, high-performance frontends." />
+      </div>
   )
 }
 
